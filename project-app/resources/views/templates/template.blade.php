7445eebaf590e190e988f6edc3454da9f6cc6019
unknown <antoniobatistajr@gmail.com>
2021-06-13 09:48:42 -0300

    Fim de personalização do pag padrao

--- project-app/resources/views/templates/template.blade.php
+++ project-app/resources/views/templates/template.blade.php
@@ -8,6 +8,30 @@
     <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
 </head>
 <body>
+    <main>
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse">
+                <ul class="navbar-nav text">
+                    <li class="nav-brand">
+                    <a class="navbar-brand" href="">LOGO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{url("Clientes")}}" class="nav-link">CLIENTES</a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="{{url("Books")}}" class="nav-link">LIVROS</a>
+                    </li>
+                </ul>          
+                </div>
+            </div>
+        </nav>
+    </main>
     @yield('content')
+    <footer class="text-center modal-footer bg-dark text-white">
+    <p>Desenvolvedor Antonio Batista de Oliveira Junior<i class="far fa-copyright"></i></p>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/ff27582da3.js" crossorigin="anonymous"></script>
 </body>
 </html>
